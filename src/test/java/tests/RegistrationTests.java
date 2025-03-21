5dcba893bba9d9d05747c71a40b621ed15597400
KravchukKat <ka.tarina@mail.ru>
2025-03-21 18:30:18 +0300

    Added HomeTest

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -5,43 +5,46 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class TextBoxTests {
+public class RegistrationTests {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
     @Test
-    void fillFormTest() {
+    void succsessfulRegistrationTest() {
         open("/automation-practice-form");
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Egorov");
         $("#userEmail").setValue("alex@egorov.com");
-        $("#genterWrapper .custom-control-label").click();
+        $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("+79227751265");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day--020").click();
-        $(".subjects-auto-complete__value-container").click();
-        $("#subjectsInput").setValue("Bla-bla-bla");
-        $("#hobbiesWrapper .custom-control-label").click();
-        $("#uploadPicture").setValue("C:/Users/Lenovo user/Downloads/948f2396b347e7303af2cbddb95d.jpg");
+        $(".react-datepicker__day--020:not(.react-datepicker__day--outside-month)").click();
+        $(".subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("C:/Users/Lenovo user/Downloads/948f2396b347e7303af2cbddb95d.jpg");
         $("#currentAddress").setValue("Some street 1");
         $("#state").click();
-        $(byText("Uttar Pradesh")).click();
+        $("stateCity-wrapper").$(byText("Uttar Pradesh")).click();
         $("#city").click();
-        $(byText("Agra")).click();
+        $("stateCity-wrapper").$(byText("Agra")).click();
         $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"), text("alex@egorov.com"), text("+79227751265"));
 
     }
 }
