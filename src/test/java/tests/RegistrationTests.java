397ca8f4cc5f9954e943b6dd83c3a7f6c9e7fb5c
KravchukKat <ka.tarina@mail.ru>
2025-03-26 20:06:59 +0300

    mistake-2

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -34,17 +33,23 @@ public class RegistrationTests {
         $(".react-datepicker__day--020:not(.react-datepicker__day--outside-month)").click();
         $(".subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
-        $("#uploadPicture").uploadFromClasspath("5f660a673d5ee92d937babfe-1.jpg");
+        $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("Some street 1");
-        $("#state").click();
-        $("stateCity-wrapper").$(byText("Uttar Pradesh")).click();
-        $("#city").click();
-        $("stateCity-wrapper").$(byText("Agra")).click();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
 
-        $(".modal-dialog").should(appear);
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"), text("alex@egorov.com"), text("+79227751265"));
+        $(".table-responsive").$(byText("Name")).sibling(0).shouldHave(exactText("Alex" + " " + "Egorov"));
+        $(".table-responsive").$(byText("Email")).sibling(0).shouldHave(text("alex@egorov.com"));
+        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text("Other"));
+        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text("89227751265"));
+        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("12 January,1990"));
+        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text("Math"));
+        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("cat.jpg"));
+        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text("Some street 1"));
+        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text("NCR Noida"));
+        $("#closeLargeModal").click();
 
     }
 }
