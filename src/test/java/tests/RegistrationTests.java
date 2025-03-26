3b2d13cda48b72012d658b48dadb06a1f89512df
KravchukKat <ka.tarina@mail.ru>
2025-03-26 22:57:50 +0300

    corrected - successful corrected - #subjectsInput

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -13,14 +13,14 @@ public class RegistrationTests {
 
     @BeforeAll
     static void beforeAll() {
-        Configuration.browserSize = "1920x1080";
+//        Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
     @Test
-    void succsessfulRegistrationTest() {
+    void successfulRegistrationTest() {
         open("/automation-practice-form");
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Egorov");
@@ -31,7 +31,7 @@ public class RegistrationTests {
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__day--020:not(.react-datepicker__day--outside-month)").click();
-        $(".subjectsInput").setValue("Math").pressEnter();
+        $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("cat.jpg");
         $("#currentAddress").setValue("Some street 1");
@@ -39,16 +39,16 @@ public class RegistrationTests {
         $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
 
-        $(".table-responsive").$(byText("Name")).sibling(0).shouldHave(exactText("Alex" + " " + "Egorov"));
-        $(".table-responsive").$(byText("Email")).sibling(0).shouldHave(text("alex@egorov.com"));
-        $(".table-responsive").$(byText("Gender")).sibling(0).shouldHave(text("Other"));
-        $(".table-responsive").$(byText("Mobile")).sibling(0).shouldHave(text("89227751265"));
-        $(".table-responsive").$(byText("Date of Birth")).sibling(0).shouldHave(text("12 January,1990"));
-        $(".table-responsive").$(byText("Subjects")).sibling(0).shouldHave(text("Math"));
-        $(".table-responsive").$(byText("Hobbies")).sibling(0).shouldHave(text("Sports"));
-        $(".table-responsive").$(byText("Picture")).sibling(0).shouldHave(text("cat.jpg"));
-        $(".table-responsive").$(byText("Address")).sibling(0).shouldHave(text("Some street 1"));
-        $(".table-responsive").$(byText("State and City")).sibling(0).shouldHave(text("NCR Noida"));
+        $(".table-responsive").$(byText("Name")).parent().shouldHave(exactText("Alex" + " " + "Egorov"));
+        $(".table-responsive").$(byText("Email")).parent().shouldHave(text("alex@egorov.com"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Other"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("89227751265"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("12 January,1990"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Math"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("cat.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Some street 1"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Noida"));
         $("#closeLargeModal").click();
 
     }
